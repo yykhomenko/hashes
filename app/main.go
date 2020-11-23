517e3ac51dc7afd834cd8cd0d187f0cd9776c32e
yykhomenko <yykhomenko@gmail.com>
2020-11-23 13:53:10 +0200

    balance done

--- app/main.go
+++ app/main.go
@@ -4,13 +4,30 @@ import (
 	"fmt"
 	"html"
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("GET %s", html.EscapeString(r.URL.Path))
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		ifaces, _ := net.Interfaces()
+		// handle err
+		for _, i := range ifaces {
+			addrs, _ := i.Addrs()
+			// handle err
+			for _, addr := range addrs {
+				var ip net.IP
+				switch v := addr.(type) {
+				case *net.IPNet:
+					ip = v.IP
+				case *net.IPAddr:
+					ip = v.IP
+				}
+				// process IP address
+				fmt.Fprintf(w, "Hello, %q, IP: %s\n", html.EscapeString(r.URL.Path), ip)
+			}
+		}
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {

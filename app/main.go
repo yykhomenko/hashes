8522aae9bcf9a4dbf37c2299880e1ff22625a786
yykhomenko <yykhomenko@gmail.com>
2020-11-23 20:39:33 +0200

    ref

--- app/main.go
+++ app/main.go
@@ -4,36 +4,18 @@ import (
 	"fmt"
 	"html"
 	"log"
-	"net"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("GET %s", html.EscapeString(r.URL.Path))
-		ifaces, _ := net.Interfaces()
-		// handle err
-		for _, i := range ifaces {
-			addrs, _ := i.Addrs()
-			// handle err
-			for _, addr := range addrs {
-				var ip net.IP
-				switch v := addr.(type) {
-				case *net.IPNet:
-					ip = v.IP
-				case *net.IPAddr:
-					ip = v.IP
-				}
-				// process IP address
-				fmt.Fprintf(w, "Hello, %q, IP: %s\n", html.EscapeString(r.URL.Path), ip)
-			}
-		}
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
 	})
 
-	log.Println("listen http :80...")
+	log.Println("listen http :80")
 	log.Fatal(http.ListenAndServe(":80", nil))
 }

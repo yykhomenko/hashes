1970c220663613735bbcbd0a6f96fd8229376cb1
yykhomenko <yykhomenko@gmail.com>
2020-11-23 03:45:56 +0200

    added load balancing

--- app/main.go
+++ app/main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
@@ -17,6 +16,6 @@ func main() {
 		fmt.Fprintf(w, "Hi")
 	})
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+	log.Println("listen http :5000...")
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }

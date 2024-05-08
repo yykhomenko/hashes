42fba86349174624a730a7116eb7ce722b954b38
Yurii Khomenko <yykhomenko@gmail.com>
2024-05-08 13:55:11 +0200

    rewrite to fiber

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,9 +6,13 @@ import (
 )
 
 type Config struct {
-	NDCS   []int  `env:"HASHES_NDCS" envSeparator:"," envDefault:"50"`
-	NDCCap int    `env:"HASHES_NDC_CAPACITY" envDefault:"10000000"`
-	Salt   string `env:"HASHES_SALT" envDefault:"mySalt"`
+	Addr         string `env:"HASHES_ADDR" envSeparator:"," envDefault:":8080"`
+	CC           string `env:"HASHES_CC" envSeparator:"," envDefault:"380"`
+	NDCS         []int  `env:"HASHES_NDCS" envSeparator:"," envDefault:"50"`
+	NDCCap       int    `env:"HASHES_NDC_CAPACITY" envDefault:"10000000"`
+	Salt         string `env:"HASHES_SALT" envDefault:"mySalt"`
+	MsisdnLenMin int    `env:"HASHES_MSISDN_LENGTH_MIN" envDefault:"12"`
+	MsisdnLenMax int    `env:"HASHES_MSISDN_LENGTH_MIN" envDefault:"21"`
 }
 
 func New() *Config {
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,23 +1,30 @@
 package server
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
 	"github.com/yykhomenko/hashes/pkg/config"
 	"github.com/yykhomenko/hashes/pkg/store"
+	"log"
+	"strconv"
+	"sync/atomic"
 )
+
+type Server struct {
+	config  *config.Config
+	store   *store.Store
+	counter *counter
+}
 
 type counter struct {
 	hashes  uint64
 	msisdns uint64
 }
 
-type Server struct {
-	config  *config.Config
-	store   *store.Store
-	counter *counter
+type response struct {
+	Value    string `json:"value,omitempty"`
+	ErrorID  byte   `json:"errorID,omitempty"`
+	ErrorMsg string `json:"errorMsg,omitempty"`
 }
 
 func New(c *config.Config, s *store.Store) *Server {
@@ -29,12 +36,85 @@ func New(c *config.Config, s *store.Store) *Server {
 }
 
 func (s *Server) Start() {
-	log.Println("http-Server listening...")
+	log.Println("http-server listening...")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/metrics", s.getMetrics).Methods("GET")
-	r.HandleFunc("/hashes/{msisdn}", s.getHash).Methods("GET")
-	r.HandleFunc("/msisdns/{hash}", s.getMsisdn).Methods("GET")
+	app := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+	})
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Get("/metrics", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).SendString(fmt.Sprintf(
+			"hashes_total %d\nmsisdns_total %d\n",
+			s.counter.hashes,
+			s.counter.msisdns,
+		))
+	})
+
+	app.Get("/hashes/:msisdn", func(c *fiber.Ctx) error {
+		atomic.AddUint64(&s.counter.hashes, 1)
+		msisdn := c.Params("msisdn")
+
+		if !validateMsisdnLen(msisdn, s.config.MsisdnLenMin, s.config.MsisdnLenMax) {
+			return c.Status(fiber.StatusBadRequest).JSON(response{ErrorID: 2, ErrorMsg: "Not supported MSISDN format: " + msisdn})
+		}
+
+		if cc, ok := validateCC(msisdn, s.config.CC); !ok {
+			return c.Status(fiber.StatusBadRequest).JSON(response{ErrorID: 3, ErrorMsg: "Not supported CC: " + cc})
+		}
+
+		if ndc, ok := validateNDC(msisdn, s.config.NDCS); !ok {
+			return c.Status(fiber.StatusBadRequest).JSON(response{ErrorID: 4, ErrorMsg: "Not supported NDC: " + ndc})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(response{Value: s.store.Hash(msisdn[3:])})
+	})
+
+	app.Get("/msisdns/:hash", func(c *fiber.Ctx) error {
+		atomic.AddUint64(&s.counter.msisdns, 1)
+		hash := c.Params("hash")
+		msisdn, exists := s.store.Msisdn(hash)
+
+		if !exists {
+			return c.Status(fiber.StatusNotFound).JSON(response{ErrorID: 1, ErrorMsg: "Not found"})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(response{Value: s.config.CC + msisdn})
+	})
+
+	log.Fatal(app.Listen(s.config.Addr))
+}
+
+func validateMsisdnLen(msisdn string, min, max int) bool {
+	l := len(msisdn)
+	return min <= l && l <= max
+}
+
+func validateCC(msisdn, confCC string) (string, bool) {
+	cc := msisdn[:3]
+	if cc == confCC {
+		return cc, true
+	} else {
+		return cc, false
+	}
+}
+
+func validateNDC(msisdn string, ndcs []int) (string, bool) {
+	ndcStr := msisdn[3:5]
+
+	ndc, err := strconv.Atoi(ndcStr)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, n := range ndcs {
+		if ndc == n {
+			return ndcStr, true
+		}
+	}
+
+	return ndcStr, false
 }
